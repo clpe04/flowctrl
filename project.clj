ff8deedb1e04f91496755044c1f5399f4d697778
Claus Engel-Christensen <cec@casalogic.dk>
2013-04-18 08:58:36 +0200

    Added loging to the project

--- project.clj
+++ project.clj
@@ -5,4 +5,10 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [enlive "1.1.1"]
-                 [criterium "0.3.1"]])
+                 [criterium "0.3.1"]
+                 [org.clojure/tools.logging "0.2.3"]
+                 [log4j "1.2.15" :exclusions [javax.mail/mail
+                                              javax.jms/jms
+                                              com.sun.jdmk/jmxtools
+                                              com.sun.jmx/jmxri]]
+                 [org.slf4j/slf4j-log4j12 "1.6.6"]])
